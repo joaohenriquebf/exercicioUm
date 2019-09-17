c29519ee5873adfc242691e22d5d68cec86612a9
pauloUbots <paulo@ubots.com.br>
2019-09-16 21:44:34 -0300

    primeiro commit

---
+++ src/test/java/LinkedListTest.java
@@ -0,0 +1,17 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LinkedListTest {
+
+    private LinkedList linkedList;
+
+    @Before
+    void setup() {
+        this.linkedList = new LinkedList();
+    }
+
+    @Test
+    void shouldReturnEmptyListSize() {
+    }
+}
